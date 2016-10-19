601b5fb2a7656efd218398634d050d4057a63596
Claudia29 <claudicre29@gmail.com>
2016-10-18 22:04:18 -0500

    l,;

---
+++ CM215/01/sketch.js
@@ -0,0 +1,42 @@
+document.ontouchmove = function(event){
+  event.preventDefault();
+}
+
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+  ellipseMode(CENTER);
+  stroke(0);
+  noFill();
+  strokeWeight(5);
+}
+
+function draw() {
+  background(255);
+  
+  for(var i=0;i<touches.length;i++){
+    if((i+1)<touches.length){
+    beginShape();
+      vertex(touches[i].x,touches[i].y);
+      vertex(touches[i].x,touches[i].y);
+      vertex(touches[i+1].x,touches[i+1].y);
+      vertex(touches[i+1].x,touches[i+1].y);
+    endShape(CLOSE);
+    }
+  else{ 
+    beginShape();
+      vertex(touches[i].x,touches[i].y);
+      vertex(touches[i].x,touches[i].y);
+      vertex(touches[i+1].x,touches[i+1].y);
+      vertex(touches[i+1].x,touches[i+1].y);
+    endShape(CLOSE);
+  }
+ 
+    ellipse(touches[i].x,touches[i].y,150,150);
+   
+  }
+}
+
+//function touchStarted() {
+  
+ //fill(random(0,255),random(0,255),random(0,255)); 
+//}
