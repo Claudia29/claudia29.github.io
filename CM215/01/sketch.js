9b9093bfde4090ab998c8f900c33c47a3ad6f46a
Claudia29 <claudicre29@gmail.com>
2016-10-19 09:14:03 -0500

    h

--- CM215/01/sketch.js
+++ CM215/01/sketch.js
@@ -13,23 +13,17 @@ function setup() {
 function draw() {
   background(255);
   
-  
+  for(var i=0;i<touches.length;i++){
     ellipse(touches[i].x,touches[i].y,150,150);
-    
+  }
 
-    //if((i+1)<touches.length){
+    fill(0,0,255);
     beginShape();
     for(var i=0;i<touches.length;i++){
       vertex(touches[i].x,touches[i].y);
     }
     endShape(CLOSE);
-  // }
-  //else{ 
-         
-    //beginShape();
-    //vertex(touches[0].x,touches[0].y);
- // endShape(CLOSE);
-    //}
+  
  
    
    
