f61625828ac00b50d23634df3b23ff1e91a6d01e
Claudia29 <claudicre29@gmail.com>
2016-10-26 14:05:22 -0500

    hhj

--- CM215/02/sketch.js
+++ CM215/02/sketch.js
@@ -1,8 +1,16 @@
 var ballSize;
+var Vx;
+var Px, Py;
+var Ax, Ay;
+var F;
 function setup() {
  createCanvas(windowWidth,windowHeight);
  ellipseMode(CENTER);
  ballSize = 50;
+ Px = windowWidth/2;
+ Vx = 0;
+ F = 0,01;
+ 
 }
 
 function draw() {
@@ -13,9 +21,21 @@ function draw() {
  text("Rx: " + floor(rotationX), 100, 100);
  text("Ry: " + floor(rotationY), 100, 150);
  text("Rz: " + floor(rotationZ), 100, 200);
+ 
+ Ax = rotationY * F;
+ Vx = Vx + Ax;
+ Px = Px + Vx;
+ ellipse(Px, ballSize,ballSize);
+ 
+ 
+
+ 
+ 
+ 
 }
 
 function drawBall(){
   fill(125);
   ellipse(windowWidth/2, rotationX+windowHeight/2, ballSize,ballSize);
+  
 }
