b093c9a78040080bf449807b03b3b773f3b869a4
Claudia29 <claudicre29@gmail.com>
2016-11-16 08:46:10 -0600

    ty

--- CM215/02/sketch.js
+++ CM215/02/sketch.js
@@ -1,30 +1,34 @@
 var ballSize;
 var vX, vY;
-var pX, pY, pX1,pX2, pY1, pY2, pX3, pY3;
+var pX, pY;
 var aX, aY;
 var f;
-var R, r1, r2, r3;
+var R, ri;
 var score;
+var obstacles = [];
 function setup() {
  createCanvas(windowWidth,windowHeight);
  ellipseMode(CENTER);
  ballSize = 50;
 pX = windowWidth/2;
  pY = windowHeight/2;
- pX1 =random(10,windowWidth);
+ /*pX1 =random(10,windowWidth);
  pY1 = random(10,windowHeight);
  pX2 = random(10,windowWidth);
  pY2 = random(10,windowHeight);
  pX3 = random(10,windowWidth);
- pY3 = random(10,windowHeight);
+ pY3 = random(10,windowHeight);*/
  vX = 0;
  vY = 0;
  f = 0.01;
  R = ballSize/2;
- r1 = (ballSize/2)/2;
+ /*r1 = (ballSize/2)/2;
  r2 = (ballSize/2)/2;
- r3 = (ballSize/2)/2;
+ r3 = (ballSize/2)/2;*/
  score = 0;
+ for (var i=0;i<3; i++){
+  obstacles[i] = new Obstacle("malus");
+  }
 }
 
 function draw() {
@@ -53,29 +57,47 @@ function draw() {
    vY = -vY;
  }
  
-if (dist(pX,pY, pX2,pY2) <= (R + r2)) {
+if (dist(pX,pY, this.xPos,this.yPos) <= (R + this.rayon)) {
    //text("s!", width / 2, height / 2, 200, 50);
    score += 10;
  }
  
- if(dist(pX,pY, pX2,pY2) <= (R + r2)){
+ /*if(dist(pX,pY, pX2,pY2) <= (R + r2)){
    //text("GA!", width / 2, height / 2, 200, 50);
    score += -10;
  }
  if(dist(pX,pY, pX3,pY3) <= (R + r3)){
    //text("GAME!", width / 2, height / 2, 200, 50);
    score += 1;
- }
+ }*/
  
 }
 
 function drawBall(){
   fill(125);
   ellipse(pX, pY, ballSize,ballSize);
-  fill(0,255,0);
+  /*fill(0,255,0);
   ellipse(pX1, pY1, ballSize/2,ballSize/2);
   fill(255,255,0);
   ellipse(pX2, pY2, ballSize/2,ballSize/2);
   fill(255,0,0);
-  ellipse(pX3, pY3, ballSize/2,ballSize/2);
+  ellipse(pX3, pY3, ballSize/2,ballSize/2);*/
+  
+  for (var i=0;i<3; i++){
+  obstacles[i].display();
+  }
+}
+
+function Obstacle(kind){
+ this.type = kind;
+ this.xPos = random(0,windowWidth);
+ this.yPos = random(0,windowHeight);
+ this.size = random(20,50);
+ this.color = color(random(0,255),random(0,255),random(0,255));
+ this.rayon = random(20,50)/2;
+ 
+ this.display = function(){
+   fill(this.color);
+   ellipse(this.xPos,this.yPos,this.size,this.size);
+ }
 }
