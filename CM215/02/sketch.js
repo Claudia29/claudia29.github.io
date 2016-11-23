dd5a374228a31582cf80d1087e59d360f0d6e021
Claudia29 <claudicre29@gmail.com>
2016-11-22 18:47:36 -0600

    fg

--- CM215/02/sketch.js
+++ CM215/02/sketch.js
@@ -39,7 +39,7 @@ function draw() {
  background(255);
  drawBall();
   
- textSize(20);
+ textSize(40);
  text("Rx: " + floor(rotationX), 100, 100);
  text("Ry: " + floor(rotationY), 100, 150);
  //text("Rz: " + floor(rotationZ), 100, 200);
